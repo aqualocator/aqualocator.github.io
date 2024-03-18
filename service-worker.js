2949a6c67478d6954fd0c4f5cf32cb96b1427b77
Chuck Konkol <ckonkol@gmail.com>
2024-03-18 15:36:03 -0500

    cc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e909ab9acf28c2186b6f86aa1a22e9a.js"
+  "/precache-manifest.830cc24a5caa3fff1202b847be73e776.js"
 );
 
 workbox.clientsClaim();
