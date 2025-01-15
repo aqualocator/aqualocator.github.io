03fad829bdfee9c8d78004988ae116d7cbe851fa
Chuck Konkol <myaquaapps@gmail.com>
2025-01-15 12:09:38 -0600

    cc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13c6f7497ebc316fb1d26c5243a622a8.js"
+  "/precache-manifest.a0c47606d39e259b8cd9ba9451ee4538.js"
 );
 
 workbox.clientsClaim();
